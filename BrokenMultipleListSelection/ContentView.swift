f16a8b1851849bb685ecfca6d7a1b1d7c207dc19
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-15 10:33:48 -0500

    • Added demo

--- BrokenMultipleListSelection/ContentView.swift
+++ BrokenMultipleListSelection/ContentView.swift
@@ -8,14 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = Set<Int>()
+    @State private var viewType = ViewType.working
+    
+    enum ViewType: String, CaseIterable{
+        case working, broken
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Picker("View Type", selection: $viewType){
+                    ForEach(ViewType.allCases, id: \.self) { viewType in
+                        Text(viewType.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding()
+                .onChange(of: viewType) { _ in
+                    selection.removeAll()
+                }
+                
+                switch viewType {
+                case .working:
+                    WorkingList(selection: $selection)
+                case .broken:
+                    BrokenList(selection: $selection)
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+            .navigationTitle("Selected (\(selection.count))")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WorkingList: View {
+    @Binding var selection: Set<Int>
+    
+    var body: some View {
+        List(selection: $selection){
+            ForEach(0..<20){ i in
+                Text("This is row \(i)")
+                    .contextMenu{
+                        Button("Print Title"){
+                            print("This is row \(i)")
+                        }
+                    }
+            }
+        }
+    }
+}
+
+
+
+struct BrokenList: View {
+    @Binding var selection: Set<Int>
+    
+    var body: some View {
+        List(selection: $selection){
+            ForEach(0..<20){ i in
+                NavigationLink {
+                    Text("This is row \(i)")
+                } label: {
+                    Text("This is row \(i)")
+                }
+                .contextMenu{
+                    Button("Print Title"){
+                        print("This is row \(i)")
+                    }
+                }
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # BrokenMultipleListSelection
+
+Demonstrates how having a Context Menu on a NavigationLink breaks the lists multiple selection.
+
+**System Info:**
+* Xcode 13.1 (13A1030d)
+* iPad Pro 11 (2018, iOS 15.1)
+* iPhone 13 Simulator
+
+
+**Reproduction steps:**
+1. Run app on an iPhone in the simulator
+2. In "working" mode, hit "Edit"
+3. Start selecting items
+4. Hit "Done"
+5. Change to "broken" mode
+6. Hit "Edit"
+7. Start selecting items
+
+
+**Actual Results:**
+In "working" mode, the selection works properly. In "broken" mode, you can see the list flicker when items are selected and then the highlight state disappears.
+
+This is caused by having the list items wrapped in a NavigationLink with a Context Menu.
+
+
+**Expected Results:**
+Both modes should work as it does in "working" mode.
