3c9dbe6d7c0593532eeb8bb9b702eaf2bd6c1da8
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-15 10:06:50 -0500

    Initial Commit

---
+++ BrokenMultipleListSelection/BrokenMultipleListSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokenMultipleListSelectionApp.swift
+//  BrokenMultipleListSelection
+//
+//  Created by Richard Witherspoon on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BrokenMultipleListSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
